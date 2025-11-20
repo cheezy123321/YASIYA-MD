16206b287dd179f1c4941b970aed196f55f298d9
cheezy123321 <usagithub20@gmail.com>
2025-11-20 06:56:16 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,5 +8,5 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || 'YASIYA-MD?mEdCDy'
+    SESSION_ID: process.env.SESSION_ID || 'YASIYA-MD?VBdOVP'
 };
